7888f720a2aa5290d5582e9e81ae8a8468f9cc24
DEIL111428 <fred23215@yandex.ru>
2025-10-08 10:29:19 +0300

    Update tests.py

--- 2 курс/Программирование ЛР-Python/ЛР-2/tests.py
+++ 2 курс/Программирование ЛР-Python/ЛР-2/tests.py
@@ -11,13 +11,14 @@ class TestMath(unittest.TestCase):
     def test_sum_of_two_3(self):
         self.assertEqual(sum_of_two([3, 3], 6), [0, 1])
 
-    def test_sum_of_two_no_result(self):
+    def test_sum_of_two_4(self):
         self.assertEqual(sum_of_two([1, 2, 3], 10), [])
 
-    def test_sum_of_two_negative_numbers(self):
+    def test_sum_of_two_5(self):
         self.assertEqual(sum_of_two([-1, -2, -3, 4], 1), [2, 3])
 
-    def test_sum_of_two_duplicates(self):
+    def test_sum_of_two_6(self):
         self.assertEqual(sum_of_two([1, 2, 2, 3], 4), [0, 3])
 
 unittest.main(argv=[''], verbosity=2, exit=False)
+
