9b718b64b4413250c85391f1fd6031ed98e5a909
DEIL111428 <fred23215@yandex.ru>
2025-11-04 19:31:25 +0300

    Refactor binary tree generation and printing

--- 2 курс/Программирование/ ЛР-Python/ЛР-3/LR_3.py
+++ 2 курс/Программирование/ ЛР-Python/ЛР-3/LR_3.py
@@ -1,42 +1,34 @@
-def gen_bin_tree(height=4, root=8):
-    if height == 0:
+import json
+from typing import Callable, Dict, Optional
+
+def gen_bin_tree(
+    height: int = 4,
+    root_value: float = 8,
+    left_branch: Callable = lambda x: x + x/2,
+    right_branch: Callable = lambda x: x ** 2
+) -> Optional[Dict]:
+    
+    if height < 0:
         return None
-    left_leaf = root + root / 2
-    right_leaf = root ** 2
-    return {
-        'value': root,
-        'left': gen_bin_tree(height - 1, left_leaf),
-        'right': gen_bin_tree(height - 1, right_leaf)
+
+    tree = {
+        'value': root_value,
+        'left': None,
+        'right': None
     }
 
-def print_tree(tree, otstup=0):
-    if tree is None:
-        return None
-    print_tree(tree['right'], otstup + 4)
-    print(' ' * otstup + f"{tree['value']}")
-    print_tree(tree['left'], otstup + 4)
+    if height > 0:
+        left_value = left_branch(root_value)
+        right_value = right_branch(root_value)
+        
+        tree['left'] = gen_bin_tree(height - 1, left_value, left_branch, right_branch)
+        tree['right'] = gen_bin_tree(height - 1, right_value, left_branch, right_branch)
+
+    return tree
+
+def print_tree(tree: Dict) -> None:
+    print(json.dumps(tree, indent=4))
 
 if __name__ == "__main__":
-    tree = gen_bin_tree()
+    tree = gen_bin_tree(height=4)
     print_tree(tree)
-
-"""
-            16777216
-        4096
-            6144.0
-    64
-            9216.0
-        96.0
-            144.0
-8
-            20736.0
-        144.0
-            216.0
-    12.0
-            324.0
-        18.0
-            27.0
-"""
-
-
-
