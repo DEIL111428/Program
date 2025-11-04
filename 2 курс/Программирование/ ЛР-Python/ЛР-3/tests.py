9c49403775569aa4363e1321dc0e2b9849eaa70b
DEIL111428 <fred23215@yandex.ru>
2025-11-04 19:32:06 +0300

    Update tests for gen_bin_tree function
    Refactor tests for binary tree generation with new cases.

--- 2 курс/Программирование/ ЛР-Python/ЛР-3/tests.py
+++ 2 курс/Программирование/ ЛР-Python/ЛР-3/tests.py
@@ -1,36 +1,17 @@
 import unittest
 from LR_3 import gen_bin_tree
+
+
 class TestBinaryTree(unittest.TestCase):
-    def test_tree_1(self):
-        tree = gen_bin_tree(height=3, root=8)
-        self.assertEqual(tree['value'], 8)
-        
-    def test_tree_2(self):
-        tree = gen_bin_tree(height=3, root=8)
-        self.assertEqual(tree['left']['value'], 12.0)
+    def setUp(self):
+        self.maxDiff = None
 
-    def test_tree_3(self):
-        tree = gen_bin_tree(height=3, root=8)
-        self.assertEqual(tree['right']['value'], 64)
+    def test_zero_height_tree(self):
+        expected = {'value': 8, 'left': None, 'right': None}
+        result = gen_bin_tree(height=0, root_value=8)
+        self.assertEqual(result, expected)
 
-    def test_tree_4(self):
-        tree = gen_bin_tree(height=3, root=8)
-        self.assertEqual(tree['left']['left']['value'], 18.0)
-    
-    def test_tree_5(self):
-        tree = gen_bin_tree(height=3, root=8)
-        self.assertEqual(tree['left']['right']['value'], 144.0)
-    
-    def test_tree_6(self):
-        tree = gen_bin_tree(height=3, root=8)
-        self.assertEqual(tree['right']['left']['value'], 96.0)
-    
-    def test_tree_7(self):
-        tree = gen_bin_tree(height=3, root=8)
-        self.assertEqual(tree['right']['right']['value'], 4096)
-
-    def test_tree_values(self):
-        tree = gen_bin_tree(height=2, root=8)
+    def test_default_tree_height_two(self):
         expected = {
             'value': 8,
             'left': {
@@ -44,7 +25,34 @@ class TestBinaryTree(unittest.TestCase):
                 'right': None
             }
         }
-        self.assertEqual(tree, expected)
-    
+        result = gen_bin_tree(height=1, root_value=8)
+        self.assertEqual(result, expected)
 
-unittest.main(argv=[''], verbosity=2, exit=False)
+    def test_custom_functions(self):
+        expected = {
+            'value': 2,
+            'left': {
+                'value': 3,
+                'left': None,
+                'right': None
+            },
+            'right': {
+                'value': 6,
+                'left': None,
+                'right': None
+            }
+        }
+        result = gen_bin_tree(
+            height=1,
+            root_value=2,
+            left_branch=lambda x: x + 1,
+            right_branch=lambda x: x * 3
+        )
+        self.assertEqual(result, expected)
+
+    def test_negative_height(self):
+        result = gen_bin_tree(height=-1)
+        self.assertIsNone(result)
+
+if __name__ == '__main__':
+    unittest.main(argv=[''], verbosity=2, exit=False)
